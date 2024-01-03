a52ef66a87f197a9b8a1ad881c17ffdba466f79e
medhatmebed <medhat.mebed@gmail.com>
2024-01-03 04:51:23 +0200

    Initial Commit

---
+++ MasterSwiftUIBootcamp/MasterSwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasterSwiftUIBootcampApp.swift
+//  MasterSwiftUIBootcamp
+//
+//  Created by Medhat Mebed on 1/3/24.
+//
+
+import SwiftUI
+
+@main
+struct MasterSwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
